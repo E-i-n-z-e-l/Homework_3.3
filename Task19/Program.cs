653c4e3f01ba1569d6420c6b0913923c435a771c
Einzel <einzel0102@mail.ru>
2022-12-27 18:06:02 +0800

    задача 19 решена

---
+++ Task19/Program.cs
@@ -0,0 +1,25 @@
+﻿int Palindrome(int number)
+{
+    if (number < 100000 && number > 9999)
+    {
+        if (number / 10000 == number % 10 && number / 1000 % 10 == number / 10 % 10)
+        {
+            Console.WriteLine($"Число {number} является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine($"Число {number} не является палиндромом");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введенное число не пятизначное");
+    }
+    return number;
+}
+
+Console.WriteLine("Введите пятизначное число ");
+int number55555 = Convert.ToInt32(Console.ReadLine());
+
+int numberPalindrome = Palindrome(number55555);
+
