a8eb8c8d37c48ab07fdf7ca9c16b588da3b36b58
Einzel <einzel0102@mail.ru>
2022-12-27 19:24:46 +0800

    решаю задачу 23

---
+++ Task23/Program.cs
@@ -0,0 +1,31 @@
+﻿int Cubing(int c_u_b_e)
+{
+    int count = 0;
+    while (count <= c_u_b_e)
+    {
+        Console.WriteLine(c_u_b_e + " | " + Math.Pow(c_u_b_e, 3));
+        count++;
+    }
+    return c_u_b_e;
+}
+
+Console.WriteLine("Введите число ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+int cuber = Cubing(cube);
+
+Console.WriteLine(cuber);
+
+
+
+//while (count <= cube)
+//{
+//    Console.WriteLine(count + " | " + Math.Pow(count, 3));
+//    count++;
+//}
+
+
+
+
+
+
